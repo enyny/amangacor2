3201422ffe7410b3810dcc1e63be9a81c4f6b631
aldry84 <aldrymanusiwa.aman@gmail.com>
2026-02-11 05:36:10 +0900

    Update JavHeyPlugin.kt

--- JavHey/src/main/kotlin/com/JavHey/JavHeyPlugin.kt
+++ JavHey/src/main/kotlin/com/JavHey/JavHeyPlugin.kt
@@ -7,14 +7,13 @@ import android.content.Context
 @CloudstreamPlugin
 class JavHeyPlugin: Plugin() {
     override fun load(context: Context) {
-        // 1. Register Provider Utama
         registerMainAPI(JavHey())
 
-        // 2. Register SEMUA Extractor Server yang kita buat tadi
         registerExtractorAPI(Hglink())
         registerExtractorAPI(Haxloppd())
-        registerExtractorAPI(Minochinos()) // Baru
-        registerExtractorAPI(Bysebuho())   // Baru
-        registerExtractorAPI(GoTv())       // Baru
+        registerExtractorAPI(Minochinos()) 
+        registerExtractorAPI(GoTv())
+        
+        // registerExtractorAPI(Bysebuho())  <-- HAPUS INI
     }
 }
